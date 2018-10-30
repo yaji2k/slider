35947f660f1c1c593e109e237821a6b738253d0c
Timur <yaji2k@gmail.com>
2018-10-30 22:01:50 +0300

    slider ready

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-    alert('test');
+    $('.slider').bxSlider({
+        pagerType: 'full',
+        nextText: '<span class="arrow right"></span>',
+        prevText: '<span class="arrow left"></span>'
+    });
+
+    $(".bx-controls").css({"margin-left": "-" + $(".bx-controls").width()/2 + "px"});
 });
